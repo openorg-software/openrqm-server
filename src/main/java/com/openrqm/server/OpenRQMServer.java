1bc879a738b32d719dada3eb03247fdaad74dda8
Marcel Jaehn <marcel.jaehn@online.de>
2019-07-24 20:48:13 +0200

    added logging with log4j

--- src/main/java/com/openrqm/server/OpenRQMServer.java
+++ src/main/java/com/openrqm/server/OpenRQMServer.java
@@ -1,4 +1,4 @@
- /*
+/*
 openrqm-server
 Entry point of the OpenRQM server implementation
 SPDX-License-Identifier: GPL-2.0-only
@@ -7,18 +7,20 @@ Copyright (C) 2019 Marcel Jaehn
 
 package com.openrqm.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * The OpenRQM server for requirements managing.
  */
 public final class OpenRQMServer {
-    private OpenRQMServer() {
-    }
+    final static Logger logger = LogManager.getLogger(OpenRQMServer.class);
 
     /**
      * Starts the OpenRQM server.
      * @param args The commandline arguments
      */
     public static void main(String[] args) {
-        System.out.println("Starting the OpenRQM server");
+        logger.info("Starting the OpenRQM server");
     }
 }
