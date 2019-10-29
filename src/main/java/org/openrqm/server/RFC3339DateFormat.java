3f12f902a1511a18a53bda1f052a2c583194d989
marceljaehn <52240760+marceljaehn@users.noreply.github.com>
2019-10-29 23:59:19 +0100

    chore: license headers (#9)
    chore: license headers

--- src/main/java/org/openrqm/server/RFC3339DateFormat.java
+++ src/main/java/org/openrqm/server/RFC3339DateFormat.java
@@ -1,11 +1,15 @@
+/*
+ * openrqm-server
+ * SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (C) 2019 Marcel Jaehn
+ */
+
 package org.openrqm.server;
 
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.fasterxml.jackson.databind.util.ISO8601Utils;
-
 import java.text.FieldPosition;
 import java.util.Date;
-
 
 public class RFC3339DateFormat extends ISO8601DateFormat {
 
