bae6ee69d8ff3341f7fbda8f5419e08110b0dc1b
Marcel Jaehn <marcel.jaehn@online.de>
2019-10-29 23:56:04 +0100

    remove html theme, append gitignore

--- src/main/java/org/openrqm/server/OpenRQMServer.java
+++ src/main/java/org/openrqm/server/OpenRQMServer.java
@@ -6,7 +6,6 @@
 
 package org.openrqm.server;
 
-import org.openrqm.export.PdfExporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
